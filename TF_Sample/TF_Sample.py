0548dcd0fdb44b1653d7c01711a463bb8f3dd76c
TaisaKiarkhi <tayisiyakiarkhi@gmail.com>
2024-01-10 12:47:56 +0100

    testing

--- TF_Sample/TF_Sample.py
+++ TF_Sample/TF_Sample.py
@@ -1,1 +1,14 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+
+x = tf.constant(4, shape = (1,1), dtype=tf.float32)
+print(x)
+
+x_ = tf.constant([1,2,3])
+y_ = tf.constant([9,8,7])
+
+z = tf.add(x_, y_)
+z = tf.subtract(x_, y_)
 
